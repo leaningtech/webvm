f3fbf4a29f7d8a660a7a69f9bf3d06a575ba3560
Alessandro Pignotti <a.pignotti@sssup.it>
2024-09-29 11:10:21 +0200

    Import tailwind to achieve full visual consistency with labs

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+import { spawn } from 'child_process';
+import svelte from 'rollup-plugin-svelte';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'build/bundle.js'
+	},
+	plugins: [
+		svelte({
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		postcss({
+			extract: true,
+			plugins: [
+				tailwindcss,
+				autoprefixer,
+			],
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte'],
+			exportConditions: ['svelte']
+		}),
+		commonjs(),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
