1f16f35ef24ef55c24a86c1e30794ec5560567a5
Alessandro Pignotti <a.pignotti@sssup.it>
2024-09-30 16:11:43 +0200

    Stub SideBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Nav from 'labs/packages/global-navbar/src/Nav.svelte';
+	import SideBar from './SideBar.svelte';
 </script>
 
 <main class="flex flex-col w-full h-full">
 	<Nav />
 	<div class="flex flex-row flex-grow">
+		<SideBar />
 		<h1>Hello</h1>
 		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	</div>
---
+++ src/SideBar.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-row w-14 bg-neutral-700">
+	<div class="flex flex-col">
+	</div>
+</div>
