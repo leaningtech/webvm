978187d61b871b12152c497a636fe1e8dae2724d
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-02 22:51:36 +0200

    Introduce xterm.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,34 @@
 <script>
+	import { onMount } from 'svelte';
+	import { Terminal } from '@xterm/xterm';
+	import { FitAddon } from '@xterm/addon-fit';
+	import { WebLinksAddon } from '@xterm/addon-web-links';
 	import Nav from 'labs/packages/global-navbar/src/Nav.svelte';
 	import SideBar from './SideBar.svelte';
+
+	var term = new Terminal({cursorBlink:true, convertEol:true, fontFamily:"monospace", fontWeight: 400, fontWeightBold: 700});
+	var fitAddon = new FitAddon();
+	term.loadAddon(fitAddon);
+	var linkAddon = new WebLinksAddon();
+	term.loadAddon(linkAddon);
+
+	onMount(function()
+	{
+		const consoleDiv = document.getElementById("console");
+		term.open(consoleDiv);
+		term.scrollToTop();
+		fitAddon.fit();
+		window.addEventListener("resize", function(ev){ fitAddon.fit(); });
+		term.focus();
+	});
 </script>
 
 <main class="flex flex-col w-full h-full">
 	<Nav />
 	<div class="flex flex-row flex-grow">
 		<SideBar />
-		<h1>Hello</h1>
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		<div class="grow h-full m-1 scrollbar" id="console">
+		</div>
 	</div>
 </main>
 
-<style>
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './global.css'
+import '@xterm/xterm/css/xterm.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.svelte';
 
