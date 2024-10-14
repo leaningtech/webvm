60b8f103b6cb522d4b7c9f6358ce5586cd1e908a
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-14 16:40:28 +0200

    Svelte: Introduce support for the alpine route

---
+++ config_public_alpine.js
@@ -0,0 +1,19 @@
+// The root filesystem location
+export const diskImageUrl = "wss://disks.webvm.io/alpine_20240307_i3_slimmed.ext2";
+// The root filesystem backend type
+export const diskImageType = "cloud";
+// Print an introduction message about the technology
+export const printIntro = false;
+// Is a graphical display needed
+export const needsDisplay = true;
+// Executable full path (Required)
+export const cmd = "/sbin/init";
+// Arguments, as an array (Required)
+export const args = [];
+// Optional extra parameters
+export const opts = {
+	// User id
+	uid: 0,
+	// Group id
+	gid: 0
+};
---
+++ src/routes/alpine/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+import WebVM from '$lib/WebVM.svelte';
+import * as configObj from '/config_public_alpine'
+</script>
+
+<WebVM configObj={configObj} />
