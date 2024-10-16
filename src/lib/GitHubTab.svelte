2f4a22a659e69a2416fe656d0e3fe687a6a1fb31
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-16 16:55:22 +0200

    Moderate GH star CTA

--- src/lib/GitHubTab.svelte
+++ src/lib/GitHubTab.svelte
@@ -4,9 +4,10 @@
 </script>
 
 <h1 class="text-lg font-bold">GitHub</h1>
-<PanelButton buttonIcon="assets/github-mark-white.svg" clickUrl="https://github.com/leaningtech/webvm" buttonText="Star on GitHub">
+<PanelButton buttonIcon="assets/github-mark-white.svg" clickUrl="https://github.com/leaningtech/webvm" buttonText="GitHub repo">
 	<i class='fas fa-star fa-xs ml-auto'></i>
 	<span class="ml-1"><GitHubStarCount repo="leaningtech/webvm"/></span>
 </PanelButton>
+<p>Like WebVM? <a class="underline" href="https://github.com/leaningtech/webvm" target="_blank">Give us a star!</a></p>
 <p>WebVM is FOSS, you can fork it to build your own version and begin working on your CheerpX-based project</p>
 <p>Found a bug? Please open a <a class="underline" href="https://github.com/leaningtech/webvm/issues" target="_blank">GitHub issue</a></p>
