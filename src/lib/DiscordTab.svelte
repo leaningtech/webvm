43992f0864a3d9863a5f5f80d6b575328f0f2461
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-16 15:51:42 +0200

    Add Discords stats in the button

--- src/lib/DiscordTab.svelte
+++ src/lib/DiscordTab.svelte
@@ -1,8 +1,15 @@
 <script>
 	import PanelButton from './PanelButton.svelte';
+	import DiscordPresenceCount from 'labs/packages/astro-theme/components/nav/DiscordPresenceCount.svelte'
 </script>
 
 <h1 class="text-lg font-bold">Discord</h1>
-<PanelButton buttonIcon="assets/discord-mark-blue.svg" clickUrl="https://discord.gg/yTNZgySKGa" buttonText="Join us on Discord"/>
+<PanelButton buttonIcon="assets/discord-mark-blue.svg" clickUrl="https://discord.gg/yTNZgySKGa" buttonText="Join our Discord">
+	<!-- This emulates the one used on labs -->
+	<svg class="inline ml-auto text-green-500" width="12" height="12">
+		<circle cx="6" cy="6" r="6" fill="currentColor" />
+	</svg>
+	<span class="ml-1"><DiscordPresenceCount /></span>
+</PanelButton>
 <p>Do you have any question about WebVM or CheerpX?</p>
 <p>Join our community, we are happy to help!</p>
--- src/lib/PanelButton.svelte
+++ src/lib/PanelButton.svelte
@@ -7,4 +7,4 @@
 	export let buttonIcon;
 </script>
 
-<a href={clickUrl} target="_blank" on:click={clickHandler} on:contextmenu={rightClickHandler}><p class="bg-neutral-700 p-2 rounded-md {(clickUrl != null || clickHandler != null) ? "hover:bg-neutral-500 cursor-pointer" : ""}" title={buttonTooltip}><img src={buttonIcon} class="inline w-8 h-8"/><span class="ml-1">{buttonText}</span></p></a>
+<a href={clickUrl} target="_blank" on:click={clickHandler} on:contextmenu={rightClickHandler}><p class="flex flex-row items-center bg-neutral-700 p-2 rounded-md {(clickUrl != null || clickHandler != null) ? "hover:bg-neutral-500 cursor-pointer" : ""}" title={buttonTooltip}><img src={buttonIcon} class="inline w-8 h-8"/><span class="ml-1">{buttonText}</span><slot></slot></p></a>
