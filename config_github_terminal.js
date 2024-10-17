a3fc89faeb9fb83ec58b5addd9316ef6a58ec99a
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-17 11:13:34 +0200

    Make sure GH config can be safely uncommented to get something that works

--- config_github_terminal.js
+++ config_github_terminal.js
@@ -9,13 +9,13 @@ export const needsDisplay = false;
 // Executable full path (Required)
 export const cmd = CMD; // Default: "/bin/bash";
 // Arguments, as an array (Required)
-export const args = ARGS; // Default: ["--login"]
+export const args = ARGS; // Default: ["--login"];
 // Optional extra parameters
 export const opts = {
 	// Environment variables
-	env: ENV, // Default: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"]
+	env: ENV, // Default: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
 	// Current working directory
-	cwd: CWD, // Default: "/home/user"
+	cwd: CWD, // Default: "/home/user",
 	// User id
 	uid: 1000,
 	// Group id
