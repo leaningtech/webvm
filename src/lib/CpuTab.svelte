a26b5230536b174684cd71501bfee3ad8c93937c
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-17 11:15:11 +0200

    Rework Engine tab

--- src/lib/CpuTab.svelte
+++ src/lib/CpuTab.svelte
@@ -1,8 +1,12 @@
 <script>
+	import PanelButton from './PanelButton.svelte';
 	import { cpuPercentage } from './activities.js'
 </script>
+
 <h1 class="text-lg font-bold">Engine</h1>
+<PanelButton buttonIcon="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="Explore CheerpX">
+</PanelButton>
 <p><span class="font-bold">Load: </span>{$cpuPercentage}%</p>
-<p>WebVM is powered by CheerpX, a x86 virtualization engine in WebAssembly</p>
-<p>CheerpX can run securely run unmodified x86 binaries and libraries in the browser</p>
-<p>For more information: <a class="text-gray-300" href="https://cheerpx.io/" target="_blank">https://cheerpx.io</a></p>
+<p>CheerpX is a x86 virtualization engine in WebAssembly</p>
+<p>It can securely run unmodified x86 binaries and libraries in the browser</p>
+<p>Excited about our technology? <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">Start building</a> your projects using <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> today!</p>
