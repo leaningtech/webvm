aa1935e389044b1f16c6af8e0b7fe86da0550893
Alessandro Pignotti <a.pignotti@sssup.it>
2024-10-18 12:12:14 +0200

    Support icons for buttons

--- src/lib/PanelButton.svelte
+++ src/lib/PanelButton.svelte
@@ -6,11 +6,14 @@
 	export let bgColor = "bg-neutral-700";
 	export let hoverColor = "hover:bg-neutral-500"
 	export let buttonImage = null;
+	export let buttonIcon = null;
 	export let buttonText;
 </script>
 
 <a href={clickUrl} target="_blank" on:click={clickHandler} on:contextmenu={rightClickHandler}><p class="flex flex-row items-center {bgColor} p-2 rounded-md shadow-md shadow-neutral-900 {(clickUrl != null || clickHandler != null) ? `${hoverColor} cursor-pointer` : ""}" title={buttonTooltip}>
 {#if buttonImage}
 <img src={buttonImage} class="inline w-8 h-8"/>
+{:else if buttonIcon}
+<i class="w-8 {buttonIcon} text-center" style="font-size: 2em;"></i>
 {/if}
 <span class="ml-1">{buttonText}</span><slot></slot></p></a>
