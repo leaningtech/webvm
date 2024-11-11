8d473a0225a02c0fabbfa1704c8df13c89c332a3
Alessandro Pignotti <a.pignotti@sssup.it>
2024-11-11 10:51:26 +0100

    Clarify label for CPU load

--- src/lib/CpuTab.svelte
+++ src/lib/CpuTab.svelte
@@ -6,7 +6,7 @@
 <h1 class="text-lg font-bold">Engine</h1>
 <PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="Explore CheerpX">
 </PanelButton>
-<p><span class="font-bold">Load: </span>{$cpuPercentage}%</p>
+<p><span class="font-bold">Virtual CPU: </span>{$cpuPercentage}%</p>
 <p>CheerpX is a x86 virtualization engine in WebAssembly</p>
 <p>It can securely run unmodified x86 binaries and libraries in the browser</p>
 <p>Excited about our technology? <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">Start building</a> your projects using <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> today!</p>
