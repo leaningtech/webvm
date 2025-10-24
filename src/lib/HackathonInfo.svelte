96ac45856542103621eb8b236f676b79f489a4c2
cidungfrog27 <codingmerlin@gmail.com>
2025-10-24 16:24:38 +0200

    reverted back to first design with minecraft font

---
+++ src/lib/HackathonInfo.svelte
@@ -0,0 +1,7 @@
+<div class="hack-info">
+    <p>
+        Join our first <a href="https://labs.leaningtech.com/cheerpj-hackathon" target="_blank">CheerpJ Hackathon</a> 
+        in the second half of November, exploring how <b>CheerpJ</b> can be applied to real-world science and research projects, with a <b>£500</b> prize awaiting the winning team. Join our <a href="https://discord.leaningtech.com" target="_blank">Discord</a> for further updates!
+    </p>
+
+</div>
