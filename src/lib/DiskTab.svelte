ae2155dc08c83615b7cbc2cf6fd3afbbcfbafdbc
Gracjan <U2364750@unimail.hud.ac.uk>
2026-01-13 16:50:30 +0000

    fixed typo in DiskTab

--- src/lib/DiskTab.svelte
+++ src/lib/DiskTab.svelte
@@ -59,4 +59,4 @@
 {/if}
 <p>WebVM runs on top of a complete Linux distribution</p>
 <p>Filesystems up to 2GB are supported and data is downloaded completely on-demand</p>
-<p>The WebVM cloud backend uses WebSockets and a it's distributed via a global CDN to minimize download latency</p>
+<p>The WebVM cloud backend uses WebSockets and it's distributed via a global CDN to minimize download latency</p>
